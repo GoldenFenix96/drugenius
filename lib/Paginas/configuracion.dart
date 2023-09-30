f75661f6fda0b3c485001e9f2b984921797c5b0f
ROBERTO CARLOS SANCHEZ DE LA CRUZ <192H17015@alumno.ujat.mx>
2023-09-30 17:59:44 -0600

    Pruebas

--- lib/Paginas/configuracion.dart
+++ lib/Paginas/configuracion.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Configuracion extends StatelessWidget {
+  const Configuracion({super.key});
 
   @override
   Widget build(BuildContext context) {
