b9789a16ef4b78281704111eb46acf23c6b4ca26
cbcarlosg <cabrerajuann@outlook.com>
2023-10-25 19:37:05 -0600

    errores

--- lib/Paginas/blank_page_foro.dart
+++ lib/Paginas/blank_page_foro.dart
@@ -7,7 +7,7 @@ class BlankPageForo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(255, 215, 135, 0.568),
+      //backgroundColor: Color.fromRGBO(255, 232, 187, 1),
       extendBodyBehindAppBar: false,
       appBar: AppBar(
         //automaticallyImplyLeading: false,
@@ -16,6 +16,17 @@ class BlankPageForo extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 22, 112, 177),
       ),
       body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromRGBO(255, 255, 255, 1),
+              Color.fromRGBO(253, 200, 66, 0.143),
+            ],
+            stops: [0.1, 0.9],
+          ),
+        ),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -26,30 +37,41 @@ class BlankPageForo extends StatelessWidget {
                 height: 250,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20.0),
-                  child: Image.asset('assets/images/FORO.png'),
+                  child: Image.asset('assets/images/FORO.PNG'),
                 ),
               ),
               const SizedBox(height: 15),
               const SizedBox(
-                child: Text('Página en desarrollo', style: TextStyle(fontSize: 18),),
+                child: Text(
+                  'Página en desarrollo',
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
               const SizedBox(height: 15),
               CircularProgressIndicator(
                 value: null,
               ),
               const SizedBox(height: 10),
-              ElevatedButton(onPressed: (){Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      const DrugeniusMenu(), // Reemplaza con la pantalla deseada
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          const DrugeniusMenu(), // Reemplaza con la pantalla deseada
+                    ),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color.fromARGB(255, 22, 112, 177),
+                  ),
                 ),
-              );},style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color.fromARGB(255, 22, 112, 177),
-      
-                          ),
-              ) ,child: Text('Volver', style: TextStyle(fontSize: 18),),)
+                child: Text(
+                  'Volver',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              )
             ],
           ),
         ),
